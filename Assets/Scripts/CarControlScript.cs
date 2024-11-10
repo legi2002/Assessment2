8ab23354ea732157155117269f7fee57d0159549
Alex <alexhill1247@gmail.com>
2024-11-10 18:38:06 +1300

    Improved car handling. Fixed audio. Fixed lighting.

--- Assets/Scripts/CarControlScript.cs
+++ Assets/Scripts/CarControlScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 // Based off the Unity manual for creating a car with wheel colliders
@@ -16,7 +17,8 @@ public class CarControl : MonoBehaviour
     WheelCollider[] wheels;
 
     public AudioSource engineSound;
-    public AudioSource brakeSound;
+    public AudioSource squealSound;
+
     public GameOverScript Over;
 
     Rigidbody rb;
@@ -29,77 +31,73 @@ public class CarControl : MonoBehaviour
     }
 
     void Update() {
+        // Inputs
         float throttleInput = Input.GetAxis("Vertical");
         float steerInput = Input.GetAxis("Horizontal");
 
+        // Speed related calculations
         float speed = Vector3.Dot(transform.forward, rb.velocity);
-
         float percentOfMaxSpeed = Mathf.InverseLerp(0, maxSpeed, speed);
+        bool isAccelerating = Mathf.Sign(throttleInput) == Mathf.Sign(speed);
 
         // Torque approaches 0 as car approaches max speed
         float currentTorque = Mathf.Lerp(motorTorque, 0, percentOfMaxSpeed);
-
         // Reduce steer angle as car approaches max speed
         float steerAngle = Mathf.Lerp(maxSteerAngle, maxSteerAngleAtSpeed, percentOfMaxSpeed);
 
-        bool isAccelerating = Mathf.Sign(throttleInput) == Mathf.Sign(speed);
-
-        if (speed < -0.1) engineSound.pitch = 1.5f;
-
-        brakeSound.volume = 0;
-
         foreach (var wheel in wheels) {
             WheelControlScript wheelParams = wheel.GetComponent<WheelControlScript>();
+
             if (wheelParams.isSteerable) {
                 wheel.steerAngle = steerInput * steerAngle;
             }
 
             if (isAccelerating) {
+                // Drive drivewheels
                 if (wheelParams.isDriveWheel) {
                     wheel.motorTorque = throttleInput * currentTorque;
                 }
                 wheel.brakeTorque = 0;
             } else {
+                // If car isn't accelerating then 'throttle' input is actually brake
                 wheel.brakeTorque = Mathf.Abs(throttleInput) * brakeTorque;
                 wheel.motorTorque = 0;
             }
 
-            // Detect wheel slip
+            // Detect wheel slip and handle audio and effects
             if (wheelParams.isDriveWheel) {
                 ParticleSystem tireSmoke = wheel.GetComponentInChildren<ParticleSystem>();
                 wheel.GetGroundHit(out WheelHit wheelHit);
-
                 bool tireSlipping = false;
-                float pitch = 1f;
 
-                Debug.Log(wheelHit.sidewaysSlip);
-                if (Math.Abs(wheelHit.sidewaysSlip) > 0.1) {
+                if (Math.Abs(wheelHit.sidewaysSlip) > 0.2) {
                     tireSlipping = true;
-                    brakeSound.volume = 0.2f;
                 }
-
-                // Slip at low forward speed
-                // Engine sound controlled here so it can max out if slipping
+                // Slip at low forward speed (not neccessarily slipping, but used for tire smoke and audio)
                 if (throttleInput > 0 && speed < 10) {
                     tireSlipping = true;
-                    pitch = 2.5f;
-                    brakeSound.volume = 0.2f;
-                } else {
-                    pitch = 2 * percentOfMaxSpeed + 0.5f;
                 }
-                engineSound.pitch = Mathf.Lerp(engineSound.pitch, pitch, Time.deltaTime);
-
                 // Slip when braking
                 if (throttleInput < 0 && speed > 0) {
                     tireSlipping = true;
-                    brakeSound.volume = 0.2f;
                 }
 
+                //--------------------Audio & Effects----------------------
+
+                float targetPitch = 2 * percentOfMaxSpeed + 0.5f;
+
                 if (tireSlipping) {
+                    // Tire slipping whilst giving throttle means engine is revving high so max pitch
+                    if (throttleInput > 0) targetPitch = 2.5f;
+                    squealSound.volume = 0.2f;
                     tireSmoke.Play();
                 } else {
+                    squealSound.volume = 0f;
                     tireSmoke.Stop();
                 }
+
+                // Smoothly transition engine pitch to target pitch
+                engineSound.pitch = Mathf.Lerp(engineSound.pitch, targetPitch, Time.deltaTime);
             }
         }
     }
